61d6788682c45ebe82e2bc43e76a39499e32e3b5
Aleksandra Fomina <sashavera1998@gmail.com>
2026-05-26 15:46:36 +0300

    modified:   bldc_inrunner_Hall-sensor-as5047_can/bldc_inrunner_Hall-sensor-as5047_can.ino

--- bldc_inrunner_Hall-sensor-as5047_can/bldc_inrunner_Hall-sensor-as5047_can.ino
+++ bldc_inrunner_Hall-sensor-as5047_can/bldc_inrunner_Hall-sensor-as5047_can.ino
@@ -2,46 +2,36 @@
 
 #include <VBCoreG4_arduino_system.h>
 #include <SimpleFOC.h>
-#include <AS5047P.h>
 
 #define AS5047P_CHIP_SELECT_PORT PA_15_ALT1
 #define AS5047P_CUSTOM_SPI_BUS_SPEED 100000
 
 //            MOSI  MISO  SCLK
 SPIClass SPI_3(PC12, PC11, PC10); 
-AS5047P as5047p(AS5047P_CHIP_SELECT_PORT, AS5047P_CUSTOM_SPI_BUS_SPEED);
 
 BLDCMotor motor = BLDCMotor(2);
 BLDCDriver3PWM driver = BLDCDriver3PWM(PA8, PA9, PA10);
-// Hall sensor instance
-// HallSensor(int hallA, int hallB , int cpr, int index)
-//  - hallA, hallB, hallC    - HallSensor A, B and C pins
-//  - pp                     - pole pairs
-HallSensor sensor = HallSensor(PB6, PC7, PC8, 2);
 
-// Interrupt routine intialisation
-// channel A and B callbacks
-void doA(){sensor.handleA();}
-void doB(){sensor.handleB();}
-void doC(){sensor.handleC();}
+// AS5047 по SPI3
+MagneticSensorSPI sensor = MagneticSensorSPI(AS5047P_CHIP_SELECT_PORT, 14, 0x3FFF);
 
 float target_voltage = 2;
 
+
 uint8_t data[4] = { 170, 171, 172, 173}; //AA AB AC AD
 unsigned long t = 0;
-FDCAN_HandleTypeDef*  hfdcan1; // создаем переменную типа FDCAN_HandleTypeDef
+FDCAN_HandleTypeDef*  hfdcan1;
 CanFD* canfd;
-FDCAN_TxHeaderTypeDef TxHeader; //FDCAN_TxHeaderTypeDef TxHeader;
+FDCAN_TxHeaderTypeDef TxHeader;
 
 void can_config(int ID){
-  SystemClock_Config();  // Настройка тактирования
-  canfd = new CanFD();  // Создаем управляющий класс
-  canfd->init(); // Инициализация CAN
-  canfd->write_default_params();  // Записываем дефолтные параметры для FDCAN (1000000 nominal / 8000000 data)
-  canfd->apply_config();  // Применяем их
-  hfdcan1 = canfd->get_hfdcan();  // Сохраняем конфиг
+  SystemClock_Config();
+  canfd = new CanFD();
+  canfd->init();
+  canfd->write_default_params();
+  canfd->apply_config();
+  hfdcan1 = canfd->get_hfdcan();
   canfd->default_start();
- 
 
   TxHeader.Identifier = ID; 
   TxHeader.DataLength = FDCAN_DLC_BYTES_4;
@@ -55,7 +45,6 @@ void setup() {
 
   pinMode(PB5, INPUT);
   pinMode(PB3, OUTPUT);
-  
 
   pinMode(PB15, OUTPUT);
   pinMode(PB14, OUTPUT);
@@ -66,30 +55,26 @@ void setup() {
   
   digitalWrite(PB3, HIGH);
 
- can_config(0x04);
+  can_config(0x04);
 
-/*----------Инициализация AS5047---------*/
-  while (!as5047p.initSPI(& SPI_3)) {
-    Serial.println(F("Can't connect to the AS5047P sensor! Please check the connection..."));
-    delay(3000);
-  }
+  /*----------Инициализация AS5047 для SimpleFOC---------*/
+  sensor.init(&SPI_3);
 
-/*-----Simple FOC init config ------*/
-  // check if you need internal pullups
- // sensor.pullup = Pullup::USE_EXTERN;
-  sensor.init();
-  sensor.enableInterrupts(doA, doB, doC);
-
-  Serial.println("Sensor ready");
+  Serial.println("AS5047 sensor ready");
 
   motor.linkSensor(&sensor);
+
   driver.voltage_power_supply = 24;
   driver.init();
+
   motor.linkDriver(&driver);
-  motor.current_limit = 6;
-  motor.init();
+  motor.current_limit = 8.7;
+
+  motor.foc_modulation = FOCModulationType::SpaceVectorPWM;
   motor.controller = MotionControlType::torque;
   motor.torque_controller = TorqueControlType::voltage;
+  motor.voltage_limit = 24;
+  motor.init();
   motor.initFOC();
 
   Serial.println(F("\n Motor ready."));
@@ -98,71 +83,63 @@ void setup() {
 }
 
 void can_send_recv(){
-//------Отправка сообщения в can------
   if (HAL_FDCAN_GetTxFifoFreeLevel(hfdcan1) != 0){
-      
-      if (HAL_FDCAN_AddMessageToTxFifoQ(hfdcan1, &TxHeader, data) != HAL_OK){ Error_Handler(); } 
-      else{digitalToggle(PD2);} //помигаем светодиодом, если все ок
+    if (HAL_FDCAN_AddMessageToTxFifoQ(hfdcan1, &TxHeader, data) != HAL_OK){ 
+      Error_Handler(); 
+    } 
+    else{
+      digitalToggle(PD2);
     }
+  }
 
-    // /*-------Получение сообщений из can-------*/ 
+  while(HAL_FDCAN_GetRxFifoFillLevel(hfdcan1, FDCAN_RX_FIFO0) > 0 )
+  {
+    FDCAN_RxHeaderTypeDef Header;
+    uint8_t RxData[4];
 
-    while(HAL_FDCAN_GetRxFifoFillLevel(hfdcan1, FDCAN_RX_FIFO0) > 0 )
-      {
-        FDCAN_RxHeaderTypeDef Header;  // хидер для входящего сообщения
-        uint8_t RxData[4]; // длина входящего сообщения - 4 байта, вообще максимальная длина сообщения - 64 байта 
-        if (HAL_FDCAN_GetRxMessage(hfdcan1, FDCAN_RX_FIFO0, &Header, RxData) != HAL_OK){ Error_Handler(); }  
-        else{ // напечатаем первые 4 байта входящего сообщения, если все ок. Пример отправки сообщения cansend can0 00000123#DEADBEEF 
-        Serial.print("ID ");
-        Serial.print(Header.Identifier); // ID сообщения 
-        Serial.print(" data: ");
-        Serial.print(RxData[0]);
-        Serial.print("  ");
-        Serial.print(RxData[1]);
-        Serial.print("  ");
-        Serial.print(RxData[2]);
-        Serial.print("  ");
-        Serial.print(RxData[3]);
-        Serial.println("  ");
-      }
-      }
+    if (HAL_FDCAN_GetRxMessage(hfdcan1, FDCAN_RX_FIFO0, &Header, RxData) != HAL_OK){ 
+      Error_Handler(); 
+    }  
+    else{
+      Serial.print("ID ");
+      Serial.print(Header.Identifier);
+      Serial.print(" data: ");
+      Serial.print(RxData[0]);
+      Serial.print("  ");
+      Serial.print(RxData[1]);
+      Serial.print("  ");
+      Serial.print(RxData[2]);
+      Serial.print("  ");
+      Serial.print(RxData[3]);
+      Serial.println("  ");
+    }
+  }
 }
-
 
 void loop() {
   if (millis() - t >= 100){
     can_send_recv();
+    motor.move(target_voltage);
+    Serial.println(target_voltage);
+    serialReceiveUserCommand();
     t = millis();
   }
 
   sensor.update();
-  Serial.print("Hall Angle: ");
-  Serial.print(sensor.getAngle());
-  Serial.print(" SPI sensor angle: ");
-  Serial.print(as5047p.readAngleDegree());
-  Serial.print(" Velocity from Hall sensor: ");
-  Serial.println(sensor.getVelocity());
-
   motor.loopFOC();
-  motor.move(target_voltage);
-  serialReceiveUserCommand();
-
- 
+  
+  
 }
 
 void serialReceiveUserCommand() {
   static String received_chars;
 
   while (Serial.available()) {
-
     char inChar = (char)Serial.read();
     received_chars += inChar;
+
     if (inChar == '\n') {
-
       target_voltage = received_chars.toFloat();
-      Serial.print("Target voltage: ");
-      Serial.println(target_voltage);
-
       received_chars = "";
     }
   }
